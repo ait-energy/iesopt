557749d13c150b4598806949efcf56f8f07bd9c4
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-06-08 21:30:59 +0200

    docs: initial setup of Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from pathlib import Path
+
+
+# -- Setup -------------------------------------------------------------------
+
+DOCS = Path(__file__).parent
+sys.path.insert(0, str((DOCS / "_extensions").resolve()))
+sys.path.insert(0, str((DOCS / ".." / "src").resolve()))
+os.environ["IESOPT_DOCS_NOEXEC"] = "true"
+
+# -- General -----------------------------------------------------------------
+
+project = "IESopt"
+copyright = "AIT Austrian Institute of Technology GmbH"
+author = "S. Strömer (@sstroemer), D. Schwabeneder (@daschw)"
+version = "1.0.0"   # TODO: get from iesopt.__version__
+
+extensions = ["sphinx.ext.autodoc", "myst_parser", "sphinx.ext.intersphinx", "julia_domain"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+source_suffix = [".md"]
+
+intersphinx_mapping = {
+    "IESopt": ("https://ait-energy.github.io/IESopt.jl/dev", None)
+}
+intersphinx_disabled_reftypes = [""]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_permalinks_icon = "<span>#</span>"
+html_theme = "sphinxawesome_theme"
+html_static_path = ["_static"]
+
+# -- MyST & autodoc configuration --------------------------------------------
+
+myst_enable_extensions = ["dollarmath", "amsmath", "colon_fence"]
+myst_heading_anchors = 3
+autodoc_packages = ["../src/iesopt"]
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+# Integrated Energy System Optimization
+
+_IESopt -- an Integrated Energy System Optimization framework._
+
+<!---
+Check the documentation for the built-in {jl:func}`IESopt.component` function.
+
+:::{toctree}
+:maxdepth: 1
+:caption: Contents:
+pages/test.md
+:::
+
+:::{admonition} This is my admonition
+This is my note
+:::
+
+```{eval-rst}
+.. autofunction:: iesopt.f_test
+.. autofunction:: iesopt.f_test2
+```
+--->
+
+It is developed and maintained at the Center for Energy at AIT Austrian Institute of Technology GmbH. The framework is designed to support the optimization of energy systems that are characterized by a high degree of integration between different energy carriers and sectors. It focuses on offering a modular and adaptable tool for modelers, that does not compromise on performance, while still being user-friendly. This is enabled by reducing energy system assets to abstract building blocks, that are supported by specialized implementation, and can be combined into complex systems without the need of a detailed understanding of mathematical modeling or proficiency in any coding-language.

1ae482e70a4a80b3b2abca25b6b918be05795859
hkoller <hannes.koller@ait.ac.at>
2025-01-30 12:11:24 +0100

    improve tag_release script

--- tag_release.sh
+++ tag_release.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 set -e
 
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[94m"
+ENDCOLOR="\e[0m"
+
 # make sure the git repo is clean
-test -z "$(git status --porcelain)" || echo "Repository is dirty. Aborting." && exit 1
+test -z "$(git status --porcelain)" || echo -e "${RED}Repository is dirty. Aborting. ${ENDCOLOR}" && exit 1
 
 # Get the current version from pyproject.toml and make sure it is a .dev version
 version="$(uvx --from=toml-cli toml get --toml-path=pyproject.toml project.version)"
 echo "Current Project version is $version"
-if [[ ! $version == *".dev"* ]]; then
-  echo "$version is no dev version. Aborting."
+if [[!  $version == *".dev"* ]]; then
+  echo -e "${RED}$version is no dev version. Aborting.${ENDCOLOR}"
   exit 1
 fi
 
 # Determine release version
 version="$(echo $version | sed 's/\.dev.*$//')"
-echo "Releasing version: $version"
+
+# Ask user
+echo -e "Release version ${GREEN} $version ${ENDCOLOR} ? [y/N]"
+read answer
+if  [ "$answer" == "${answer#[Yy]}" ] ; then
+  echo "Aborted" && exit 1
+fi
 
 # Switch pyproject.toml to release version and commit
 uvx --from=toml-cli toml set --toml-path=pyproject.toml project.version $version
@@ -32,7 +43,7 @@ echo "Starting new version: $new_version"
 uvx --from=toml-cli toml set --toml-path=pyproject.toml project.version $new_version
 git commit -am"Start developing version $new_version"
 
-echo "Tag created. Use git push --tags to make this release permanent"
+echo -e "Tag created. Use $BLUE git push --tags $ENDCOLOR to make this release permanent"
 
 
 

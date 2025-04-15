26b856679eec9e1eb72c2a5fc2f3843fc72bb08a
Daniel Schwabeneder <daniel.schwabeneder@ait.ac.at>
2025-04-15 17:05:59 +0200

    chore: bump IESopt core and julia versions

--- src/iesopt/config.py
+++ src/iesopt/config.py
@@ -12,8 +12,8 @@ def _strtobool(val: str):
 
 class Config:
     DEFAULTS = {
-        "IESOPT_JULIA": "1.11.4",
-        "IESOPT_CORE": "2.6.3",
+        "IESOPT_JULIA": "1.11.5",
+        "IESOPT_CORE": "2.6.4",
         "IESOPT_JUMP": "1.25.0",
         "IESOPT_SOLVER_HIGHS": "1.15.0",
         "IESOPT_MULTITHREADED": "no",  # yes, no

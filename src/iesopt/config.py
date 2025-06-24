94866c4fccd574d130c03fd789f2d1e8f0827186
Stefan Strömer <stefan.stroemer@ait.ac.at>
2025-06-24 21:45:59 +0200

    chore: bump IESopt.jl to v2.8.0 and HiGHS.jl to v1.18.1

--- src/iesopt/config.py
+++ src/iesopt/config.py
@@ -13,9 +13,9 @@ def _strtobool(val: str):
 class Config:
     DEFAULTS = {
         "IESOPT_JULIA": "1.11.5",
-        "IESOPT_CORE": "2.7.0",
+        "IESOPT_CORE": "2.8.0",
         "IESOPT_JUMP": "1.26.0",
-        "IESOPT_SOLVER_HIGHS": "1.17.0",
+        "IESOPT_SOLVER_HIGHS": "1.18.1",
         "IESOPT_MULTITHREADED": "no",  # yes, no
         "IESOPT_OPTIMIZATION": "latency",  # rapid, latency, normal, performance
     }

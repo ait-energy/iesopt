93a583356ed4b27664b1dd70dc83ed75f0dcdff9
hkoller <hannes.koller@ait.ac.at>
2025-09-25 11:28:20 +0200

    make to_pandas() more robust
    - to_pandas now supports variable arrays with irregular size (size != len(snapshots)
    
    - to_pandas now raises an Exception when it could not create a DataFrame (instead of returning None)
    
    - updated unittests

--- tests/test_results.py
+++ tests/test_results.py
@@ -6,12 +6,15 @@ from iesopt.results import Results
 
 
 class TestToPandas_Long:
-    def test__to_pandas_with_snapshot_variables(self, create_result):
+    def test__to_pandas_with_regular_items(self, create_result):
+        # WHEN: results has variables/constraints/expressions for every snapshot
         results = create_result(
             snapshots=[1, 2, 3],
             data={
                 ("comp1", "var", "my_var1"): [1.0, 2.0, 3.0],
                 ("comp2", "var", "my_var2"): [4.0, 5.0, 6.0],
+                ("comp2", "con", "my_con"): [7.0, 8.0, 9.0],
+                ("comp2", "exp", "my_exp"): [10.0, 11.0, 12.0],
             },
         )
         df = results.to_pandas(orientation="long")
@@ -20,17 +23,18 @@ class TestToPandas_Long:
             df,
             pd.DataFrame(
                 {
-                    "snapshot": [1, 2, 3] * 2,
-                    "component": ["comp1"] * 3 + ["comp2"] * 3,
-                    "fieldtype": ["var"] * 6,
-                    "field": ["my_var1"] * 3 + ["my_var2"] * 3,
-                    "value": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-                    "mode": ["primal"] * 6,
+                    "snapshot": [1, 2, 3] * 4,
+                    "component": ["comp1"] * 3 + ["comp2"] * 9,
+                    "fieldtype": ["var"] * 6 + ["con"] * 3 + ["exp"] * 3,
+                    "field": ["my_var1"] * 3 + ["my_var2"] * 3 + ["my_con"] * 3 + ["my_exp"] * 3,
+                    "value": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0],
+                    "mode": ["primal"] * 12,
                 }
             ),
         )
 
     def test__to_pandas_with_single_variable(self, create_result):
+        # WHEN: results has a mix of array and single variables
         results = create_result(
             snapshots=[1, 2, 3],
             data={
@@ -54,6 +58,7 @@ class TestToPandas_Long:
         )
 
     def test__to_pandas_with_dual_variable(self, create_result):
+        # WHEN: results has a variable marked as dual
         results = create_result(
             snapshots=[1, 2, 3],
             data={
@@ -71,6 +76,30 @@ class TestToPandas_Long:
                     "field": ["my_var1"] * 3,
                     "value": [1.0, 2.0, 3.0],
                     "mode": ["dual"] * 3,
+                }
+            ),
+        )
+
+    def test__to_pandas_with_irregular_array_variable(self, create_result):
+        # WHEN: a results variable is an array with len >0 and len != len(snapshots)
+        results = create_result(
+            snapshots=[1, 2, 3],
+            data={
+                ("comp1", "var", "my_var1"): [1.0, 2.0, 3.0],
+                ("comp1", "var", "my_var2"): [5.0, 10.0],
+            },
+        )
+        df = results.to_pandas(orientation="long")
+        assert_frame_equal(
+            df,
+            pd.DataFrame(
+                {
+                    "snapshot": [1, 2, 3, None, None],
+                    "component": ["comp1"] * 5,
+                    "fieldtype": ["var"] * 5,
+                    "field": ["my_var1"] * 3 + ["my_var2[0]", "my_var2[1]"],
+                    "value": [1.0, 2.0, 3.0, 5.0, 10.0],
+                    "mode": ["primal"] * 5,
                 }
             ),
         )

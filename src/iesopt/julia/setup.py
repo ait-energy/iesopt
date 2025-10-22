9fd0e0be02d9005dfd68ec33d41ca398e03fc02d
Stefan Strömer <stefan.stroemer@ait.ac.at>
2025-10-22 12:38:38 +0200

    fix: warning formatting

--- src/iesopt/julia/setup.py
+++ src/iesopt/julia/setup.py
@@ -157,9 +157,9 @@ def setup_julia(target: Path, sysimage: Path):
         if ("cluster" in hostname) or ("node" in hostname) or ("controller" in hostname):
             libdir = str((Path(juliapkg.executable()).parent / ".." / "lib" / "julia").resolve())
             logger.warning(
-                "    It seems we are running on a cluster; if you encounter an error related to 'GLIBCXX_*.*.*, "
-                "    or 'Unable to load dependent library', please manually set the LD_LIBRARY_PATH environment "
-                "    variable (e.g., by using 'export LD_LIBRARY_PATH=...')to: '%s'." % libdir
+                " It seems we are running on a cluster; if you encounter an error related to 'GLIBCXX_*.*.*, "
+                + " or 'Unable to load dependent library', please manually set the LD_LIBRARY_PATH environment "
+                + " variable (e.g., by using 'export LD_LIBRARY_PATH=...')to: '%s'." % libdir
             )
 
     os.environ["PYTHON_JULIACALL_BINDIR"] = str(Path(juliapkg.executable()).parent)
